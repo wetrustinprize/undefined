921e97bad20256a6bc9be1cfa447ebd7b5dbf641
Peterson <contact.petersonac@gmail.com>
2019-10-24 03:27:31 -0300

    New animations and animator script

--- Assets/Scripts/Player/PlayerAnimator.cs
+++ Assets/Scripts/Player/PlayerAnimator.cs
@@ -15,17 +15,40 @@ public class PlayerAnimator : MonoBehaviour
 
     //Script side vars
 
-    private Motor motor {get {return GetComponent<Motor>();}}
+    private Motor motor { get { return GetComponent<Motor>(); } }
 
         #endregion
 
     void Update() {
-        float vInput = Input.GetAxisRaw("Horizontal");
-        float vVel = 0;
+        float vInput = motor.inputAxis.x;
+        float vVel = motor.GetForce("input").ActualForce.x / motor.MaxSpeed.x;
+        float vDir = vVel > 0 ? 1 : -1;
 
-        if(vInput != 0) sprite.flipX = vInput < 0;
-        animator.SetFloat("Speed", vInput * (vInput < 0 ? -1 : 1));
-        animator.SetFloat("Walk Anim Speed", (vVel * (vVel < 0 ? -1 : 1)) / 6);
+        bool onAir = !motor.OnGround;
+        bool drifting = vInput != 0 ? (vDir != vInput) : false;
+        bool onWall = motor.OnWall && motor.HasSlow("wallSlow");
+        bool dashing = motor.HasForce("dash", false);
+
+        if(vInput != 0) {
+            bool flip = false;
+
+            if(!onWall)
+            {
+                flip = vInput < 0;
+            }
+            else
+            {
+                flip = motor.onRightWall ? false : true;
+            }
+
+            sprite.flipX = flip;
+        }
+        animator.SetFloat("SpeedH", Mathf.Abs(vVel));
+        animator.SetFloat("Input", Mathf.Abs(vInput));
+        animator.SetBool("OnAir", onAir);
+        animator.SetBool("Drift", drifting);
+        animator.SetBool("OnWall", onWall);
+        animator.SetBool("Dashing", dashing);
     }
 
 }
