3ffbefaa5fbc5ec508842dd917dd5ca48d087b56
Peterson <contact.petersonac@gmail.com>
2019-11-17 22:21:16 -0300

    New auto destroy component.

---
+++ Assets/Scripts/AutoDestroy.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class AutoDestroy : MonoBehaviour
+{
+    
+    [SerializeField]
+    private float time;
+
+    void Start()
+    {
+        
+        GameObject.Destroy(this.gameObject, time);
+
+    }
+
+}
