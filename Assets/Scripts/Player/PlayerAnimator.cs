0e09d4a7ede4a9a52b9becb6cceb8f606010d7dc
Peterson <contact.petersonac@gmail.com>
2020-01-13 01:29:00 -0300

    Wall particles and squish effect when falling

--- Assets/Scripts/Player/PlayerAnimator.cs
+++ Assets/Scripts/Player/PlayerAnimator.cs
@@ -7,53 +7,54 @@ public class PlayerAnimator : MonoBehaviour
         #region Variables
 
     [Header("Animator Settings")]
-    [SerializeField]
-    private Motor motor;
-    [SerializeField]
-    private Attack attack;
-    [SerializeField]
-    private Jump jump;
-    [SerializeField]
-    private float driftThreshold;
+    [SerializeField] private Motor motor;               // Reference to the Player Motor component
+    [SerializeField] private Attack attack;             // Reference to the Attack component
+    [SerializeField] private Jump jump;                 // Reference to the Jump component
+    [SerializeField] private float driftThreshold;      // The threshold to play the drift animation
 
     [Header("Particles")]
-    [SerializeField]
-    private GameObject dustParticles;
-    [SerializeField]
-    private GameObject dashParticles;
-    [SerializeField]
-    private Transform dustSpawnTransform;
+    [SerializeField] private GameObject dustParticles;          // The dust particles when walking/running
+    [SerializeField] private ParticleSystem wallParticles;      // The wall dust particles when walking/running
+    [SerializeField] private GameObject dashParticles;          // The dash particles when dashing
+    [SerializeField] private Transform dustSpawnTransform;      // The dust particles transform
 
     [Header("Animator References")]
-    [SerializeField]
-    private Animator animator;
-    [SerializeField]
-    private SpriteRenderer sprite;
+    [SerializeField] private Animator animator;             // Reference to the animator
+    [SerializeField] private SpriteRenderer sprite;         // Reference to the sprite renderer
 
     [Header("Camera Refereces")]
-    [SerializeField]
-    private CameraController cam;
+    [SerializeField] private CameraController cam;          // Reference to the camera controller
 
     //Script side var
 
     private bool attacked;
     private bool dashParticleSpawned;
+    private float wallParticleDefaultX;
+    private float squishPercentage;
 
         #endregion
 
     void Start() {
 
+        // Setup the attack events
         attack.onPerform += AttackAnim;
         attack.onAttack += AttackEffect;
 
+        // Sets the initial transform of the wall particle
+        wallParticleDefaultX = wallParticles.gameObject.transform.localPosition.x;
+
     }
 
+        #region Attack Events
+
+    // Called when attacks
     void AttackAnim() {
 
         attacked = true;
 
     }
 
+    // Called when hits something
     void AttackEffect() {
 
         Vector2 dir = new Vector2(0.3f * motor.lastFaceDir, 0);
@@ -61,7 +62,12 @@ public class PlayerAnimator : MonoBehaviour
 
     }
 
+        #endregion
+
+    // Called to create the dust particles
     public void DustParticles() {
+
+        if(!motor.OnGround) return;
 
         Vector3 pos = dustSpawnTransform.position;
 
@@ -79,6 +85,7 @@ public class PlayerAnimator : MonoBehaviour
 
     }
 
+    // Called to create the dash particles
     public void DashParticles() {
 
         Quaternion rot = Quaternion.identity;
@@ -92,13 +99,15 @@ public class PlayerAnimator : MonoBehaviour
 
     }
 
+    // Updates the animator
     void Update() {
         // Gets all velocity info
         float vInput = motor.inputAxis.x;
         float hVel = motor.GetForce("input").ActualForce.x / motor.MaxSpeed.x;
         float vDir = hVel > 0 ? 1 : -1;
+        float vFace = motor.lastFaceDir;
 
-        float vVel = motor.finalSpeed.y / 15;
+        float vVel = motor.finalSpeed.y;
 
         // Get all states
         bool onAir = !motor.OnGround;
@@ -108,6 +117,7 @@ public class PlayerAnimator : MonoBehaviour
         bool dashing = motor.HasForce("dash", false);
 
         // Check if should flip
+            #region Flip
         if(vInput != 0) {
             bool flip = false;
 
@@ -141,6 +151,7 @@ public class PlayerAnimator : MonoBehaviour
         {
             sprite.flipX = false;
         }
+            #endregion
 
 
         // Send values to the animator
@@ -153,6 +164,18 @@ public class PlayerAnimator : MonoBehaviour
         animator.SetBool("Dashing", dashing);
         animator.SetBool("Attack", attacked);
 
+        // Squish effect
+        squishPercentage = Mathf.Lerp(squishPercentage, Mathf.Clamp(vVel / -70, 0, 1), Time.fixedDeltaTime);
+        sprite.transform.localScale = new Vector3(1, 1 + (0.05f * squishPercentage), 1);
+
+        // Enables/disables wall particles
+        Vector2 pos = wallParticles.gameObject.transform.localPosition;
+        wallParticles.gameObject.transform.localPosition = new Vector2(wallParticleDefaultX * (sprite.flipX ? -1 : 1), pos.y);
+
+        if(!(onWall && onAir && canWallJump))
+            wallParticles.Play(true);
+
+        // Spawns and checks the dash particles
         if(!dashParticleSpawned && dashing)
         {
             dashParticleSpawned = true;
