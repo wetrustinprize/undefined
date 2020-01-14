43d708b11a1588f5c679905eb89ac8c35db7d2a2
Peterson <contact.petersonac@gmail.com>
2020-01-13 21:50:46 -0300

    New sprite parallax system.

---
+++ Assets/Scripts/SpriteParallax.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class SpriteParallax : MonoBehaviour
+{
+    
+        #region Variables
+
+    [Header("Parallax config")]
+    [SerializeField] private Vector2 effect;
+    [SerializeField] private Transform cam;
+
+    private Vector2 startPos;
+
+        #endregion
+
+    void Start() {
+        startPos = (Vector2)transform.position;
+    }
+
+    void LateUpdate() {
+
+        Vector2 pos = cam.transform.position * -effect;
+
+        transform.position = new Vector2(
+            startPos.x + pos.x,
+            startPos.y + pos.y
+        );
+
+    }
+
+}
