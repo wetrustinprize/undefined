1a8db351650c2dbc2a12dc4e10f275a8b6de767f
Peterson <contact.petersonac@gmail.com>
2020-01-30 19:13:54 -0300

    new HUD script

---
+++ Assets/Scripts/Player/HUD/HUDHPBar.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDHPBar : MonoBehaviour
+{
+        #region Variables
+
+    [Header("Bars")]
+    [SerializeField] private Image[] hpBars;   // ALl the HP bars
+    [SerializeField] private Animator hpAnimator;
+
+    [Header("Bars Texture")]
+    [SerializeField] private Sprite hpFull;
+    [SerializeField] private Sprite hpHalfBroken;
+    [SerializeField] private Sprite hpBroken;
+    [SerializeField] private float thresholdToBroken;
+
+    [Header("Particles")]
+    [SerializeField] private GameObject hpBreakParticle;
+
+    // Local variables
+    private Alive playerAliveComponent;             // The player Alive component
+    private int lastExplodedBar;
+
+        #endregion
+    void Start()
+    {
+        lastExplodedBar = 999;
+
+        playerAliveComponent = GameObject.FindWithTag("Player").GetComponent<Alive>();
+
+        // Event to run when the player gets damaged.
+        playerAliveComponent.onDamage += OnDamage;
+
+        playerAliveComponent.onHeal += OnHeal;
+
+    }
+
+    void UpdateSprites() {
+        int curHealth = playerAliveComponent.Health;
+        int maxHealth = playerAliveComponent.MaxHealth;
+
+        float percentage = Mathf.Clamp((float)curHealth / (float)maxHealth, 0, 1);
+
+        for(int i = 0; i < hpBars.Length; i++) {
+
+            float myPercentage = (1/(float)hpBars.Length) * (i + 1);
+            Debug.Log(myPercentage);
+
+            if(myPercentage > percentage + thresholdToBroken) {
+                if(i < lastExplodedBar)
+                    lastExplodedBar = i;
+                
+                hpBars[i].sprite = hpBroken;
+            }
+            else if(myPercentage > percentage + thresholdToBroken/2) {
+                hpBars[i].sprite = hpHalfBroken;
+            }
+            else
+            {
+                hpBars[i].sprite = hpFull;
+            }
+
+        }
+    }
+
+    void OnHeal() {
+
+        UpdateSprites();
+        lastExplodedBar = 999;
+
+    }
+
+    void OnDamage() {
+
+        int curHealth = playerAliveComponent.Health;
+        int maxHealth = playerAliveComponent.MaxHealth;
+
+        int lastExplodedBarReminder = lastExplodedBar;
+
+        float percentage = Mathf.Clamp((float)curHealth / (float)maxHealth, 0, 1);
+
+        UpdateSprites();
+
+        if(lastExplodedBarReminder != lastExplodedBar)
+        {
+            Instantiate(hpBreakParticle, hpBars[lastExplodedBar].transform);
+        }
+
+        hpAnimator.SetTrigger("Damaged");
+
+    }
+}
