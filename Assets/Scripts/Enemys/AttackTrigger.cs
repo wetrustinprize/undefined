72cdb334cec71ffd58fc3e25b8a16dcaaa5af85a
Peterson <contact.petersonac@gmail.com>
2020-02-12 16:53:56 -0300

    Fixed attack trigger

--- Assets/Scripts/Enemys/AttackTrigger.cs
+++ Assets/Scripts/Enemys/AttackTrigger.cs
@@ -5,15 +5,26 @@ public class AttackTrigger : MonoBehaviour {
         #region Variables
 
     [SerializeField] private Attack attack;
+    [SerializeField] private bool ignorePlayer;
 
         #endregion
 
+    void Start() {
+
+        if(attack == null)
+        {
+            if(TryGetComponent<Attack>(out Attack a)) { attack = a; }
+            else { Destroy(this); }
+        }
+
+    }
+
     void OnTriggerEnter2D(Collider2D collision) {
 
-        if(collision.tag == "Player") {
-            if(collision.TryGetComponent<Alive>(out Alive a)) {
-                attack.DirectAttack(a, collision.transform);
-            }
+        if(collision.tag == "Player" && ignorePlayer) return;
+
+        if(collision.TryGetComponent<Alive>(out Alive a)) {
+            attack.DirectAttack(a, collision.transform);
         }
 
     }
