ab824411dd0bac85e0181ef4ea17d9fa18d26f27
Peterson <contact.petersonac@gmail.com>
2020-02-24 22:03:45 -0300

    Creature animation

---
+++ Assets/Scripts/Creature/CreatureAnimator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatureAnimator : MonoBehaviour
+{
+
+        #region Variables
+
+    [SerializeField] private CreatureController creatureController;
+
+    // Script side
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+
+        #endregion
+
+
+
+    void Start() {
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+
+
+    }
+
+    void Update()
+    {
+        
+        spriteRenderer.flipX = creatureController.LastFacingDir != 1;
+        animator.SetBool("Flying", !creatureController.IsNearToThePlayer);
+
+    }
+}
