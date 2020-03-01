2e1aee39b21019317d1fe6ae86e32c12115fe8c3
Peterson <contact.petersonac@gmail.com>
2020-02-29 23:09:24 -0300

    New sound manager class

---
+++ Assets/Scripts/SoundsManager/SoundsManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+namespace Undefined.Sound {
+    public class SoundsManager : MonoBehaviour
+    {
+
+            #region Variables
+
+        [SerializeField] private AudioMixer mixer;
+
+        public static SoundsManager instance;
+
+        // Script side
+        private AudioSource uiSource;
+
+            #endregion
+
+        void Awake() {
+
+            DontDestroyOnLoad(this.gameObject);
+
+        }
+
+        void Start() {
+
+            // Static instace
+            if(instance != null) Destroy(this.gameObject);
+            
+            instance = this;
+
+            // Gets the UI AudioSource
+            uiSource = CameraController.main.GetComponent<AudioSource>();
+
+        }
+
+        ///<summar>Plays a UI SFX</summary>
+        ///<param name="sfx">The AudioClip to play</param>
+        ///<param name="vol">The SFX volume</param>
+        public static void PlayUISFX(AudioClip sfx, float vol = 1.0f) {
+
+            instance.uiSource.PlayOneShot(sfx, vol);
+
+        }
+
+    }
+
+}
