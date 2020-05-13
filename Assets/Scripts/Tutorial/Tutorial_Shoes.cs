022afc97f7ae6366d18442a71e0058790298e460
Peterson <contact.petersonac@gmail.com>
2020-05-12 23:16:10 -0300

    shoes class

---
+++ Assets/Scripts/Tutorial/Tutorial_Shoes.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Tutorial_Shoes : MonoBehaviour
+{
+    
+        #region Variables
+
+    private Animator myAnimator;
+
+        #endregion
+
+    void Start() {
+
+        this.myAnimator = GetComponent<Animator>();
+
+    }
+
+    void OnTriggerEnter2D(Collider2D col) {
+
+        if(col.tag != "Player") return;
+        col.gameObject.GetComponent<Jump>().allowWallJumps = true;
+
+        myAnimator.SetTrigger("Pickup");
+
+    }
+
+}
