59c5ac0ed269ca40ae126550d2004c52f0c3158d
Peterson <contact.petersonac@gmail.com>
2020-05-12 23:16:05 -0300

    fixed modulation with boxfade class

--- Assets/Scripts/Tutorial/Tutorial_Boxfade.cs
+++ Assets/Scripts/Tutorial/Tutorial_Boxfade.cs
@@ -4,23 +4,26 @@ public class Tutorial_Boxfade : MonoBehaviour
 {
         #region Variables
 
-    private Animator myAnimator;
+    [Header("Animator")]
+    [SerializeField] private Animator animator;
+
+    // script side
     private bool shouldShow = false;
 
         #endregion
 
 
     void Start() {
-        this.myAnimator = this.GetComponent<Animator>();
+        this.animator = animator == null ? this.GetComponent<Animator>() : animator;
     }
 
     void OnTriggerEnter2D(Collider2D col) {
         this.shouldShow = col.tag == "Player" ? true : this.shouldShow;
-        this.myAnimator.SetBool("fadein", shouldShow);
+        this.animator.SetBool("fadein", shouldShow);
     }
 
     void OnTriggerExit2D(Collider2D col) {
         this.shouldShow = col.tag == "Player" ? false : this.shouldShow;
-        this.myAnimator.SetBool("fadein", shouldShow);
+        this.animator.SetBool("fadein", shouldShow);
     }
 }
