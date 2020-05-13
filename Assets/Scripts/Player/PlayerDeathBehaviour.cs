bbe9de7f24e93022e1d81039bccf68a9eb940f77
Peterson <contact.petersonac@gmail.com>
2020-05-12 23:15:35 -0300

    death behaviour for the player

---
+++ Assets/Scripts/Player/PlayerDeathBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Alive))]
+public class PlayerDeathBehaviour : MonoBehaviour
+{
+    
+        #region Variables
+
+    private Alive myAlive;
+
+        #endregion
+
+
+    void Start() {
+
+        this.myAlive = this.GetComponent<Alive>();
+
+        myAlive.onDie += Death;
+
+    }
+
+    void Death() {
+
+        
+
+    }
+
+}
