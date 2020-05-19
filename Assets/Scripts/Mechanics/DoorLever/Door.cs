85f901bbacac30bd3e14ce828909345fd2a37675
Peterson <contact.petersonac@gmail.com>
2020-05-18 23:15:16 -0300

    new lever and door mechanic

---
+++ Assets/Scripts/Mechanics/DoorLever/Door.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+
+public class Door : MonoBehaviour
+{
+    
+        #region Variables
+
+    [Header("Information")]
+    [SerializeField] private bool activated;
+    [SerializeField] private BoxCollider2D doorCollider;
+
+    [Header("Positions")]
+    [SerializeField] private Transform disabledPos;
+    [SerializeField] private Transform enabledPos;
+
+    // script side
+    private Color doorColor = Color.gray;
+
+
+        #endregion
+    
+    void OnDrawGizmos() {
+
+        if(doorCollider == null) return;
+        if(disabledPos == null) return;
+        if(enabledPos == null) return;
+
+        Transform nowPos = activated ? enabledPos : disabledPos;
+        Transform goPos = activated ? disabledPos : enabledPos;
+
+        Gizmos.color = doorColor;
+
+        Matrix4x4 old = Gizmos.matrix;
+
+        Gizmos.matrix = goPos.localToWorldMatrix;
+        Gizmos.DrawWireCube(Vector3.zero, doorCollider.size);
+
+        Gizmos.matrix = old;
+        Gizmos.DrawLine(nowPos.position, goPos.position);
+
+    }
+
+}
---
+++ Assets/Scripts/Mechanics/DoorLever/Lever.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+    
+        #region Variables
+
+    [Header("Lever Settings")]
+    [SerializeField] private Color leverColor;
+
+    [Header("Doors")]
+    [SerializeField] private Door[] doorsToActivate;
+
+        #endregion
+
+    void OnDrawGizmos() {
+
+        if(doorsToActivate == null || doorsToActivate.Length <= 0) return;
+
+        foreach(Door door in doorsToActivate) {
+            Vector3 doorPos = door.transform.position;
+
+            Gizmos.color = leverColor;
+            Gizmos.DrawLine(this.transform.position, doorPos);
+        }
+
+    }
+
+
+}
