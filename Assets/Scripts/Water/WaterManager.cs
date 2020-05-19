1c77bc50bac310c05acdb62a08d0a91c4c9d8006
Peterson <contact.petersonac@gmail.com>
2020-05-18 23:14:27 -0300

    water manager class

---
+++ Assets/Scripts/Water/WaterManager.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class WaterManager : MonoBehaviour
+{
+
+        #region Variables
+
+    [Header("Reflection Camera")]
+    [SerializeField] private Camera waterReflectionCamera;
+
+    [Header("Reflection Sprite")]
+    [SerializeField] private SpriteRenderer spriteRenderer;
+
+    private RenderTexture waterReflection;
+    public static WaterManager instance;
+
+
+        #endregion
+
+
+    void LateUpdate() {
+
+        if(waterReflectionCamera == null) return;
+
+        waterReflectionCamera.backgroundColor = GameManager.Camera.Camera.backgroundColor;
+        waterReflectionCamera.orthographicSize = GameManager.Camera.Camera.orthographicSize;
+
+        Vector3 Distance = this.transform.position - GameManager.Camera.transform.position;
+
+        Vector3 ThisPos = this.transform.position;
+        float MainCamZ = GameManager.Camera.transform.position.z;
+        Vector3 NewCamPos = new Vector3(ThisPos.x - Distance.x, ThisPos.y + Distance.y, MainCamZ);
+        waterReflectionCamera.transform.position = NewCamPos;
+
+    }
+
+    void Start() {
+
+        if(instance == null)
+        {
+            instance = this;
+
+            waterReflection = new RenderTexture(Screen.width, Screen.height, 0);
+            waterReflection.filterMode = FilterMode.Point;
+
+            waterReflectionCamera.targetTexture = waterReflection;
+            spriteRenderer.material.SetTexture("_Reflection", waterReflection);
+        }
+        else
+        {
+            spriteRenderer.material.SetTexture("_Reflection", waterReflection);
+            Destroy(waterReflectionCamera);
+        }
+
+
+    }
+
+}
