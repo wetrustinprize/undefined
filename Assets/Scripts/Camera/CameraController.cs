a061a9a3e17e019188c2dc1aa245bfa93a8ec5a4
Peterson <contact.petersonac@gmail.com>
2020-05-29 03:45:42 -0300

    New function to get point from screen to world pos

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -253,6 +253,17 @@ public class CameraController : MonoBehaviour
 
     }
 
+    ///<summary>Gets mouse position in 3D space</summary>
+    ///<param name="screenPosition">Mouse position</param>
+    ///<param name="z">3D space Z</param>
+    public Vector3 GetWorldPositionOnPlane(Vector3 screenPosition, float z) {
+        Ray ray = Camera.main.ScreenPointToRay(screenPosition);
+        Plane xy = new Plane(Vector3.forward, new Vector3(0, 0, z));
+        float distance;
+        xy.Raycast(ray, out distance);
+        return ray.GetPoint(distance);
+    }
+
         #endregion
 
 }
