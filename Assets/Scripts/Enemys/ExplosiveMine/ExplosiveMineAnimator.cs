9b0d948244891e000da2db2d22a39986810a2154
Peterson <contact.petersonac@gmail.com>
2020-05-31 23:03:15 -0300

    Explosive Mine

---
+++ Assets/Scripts/Enemys/ExplosiveMine/ExplosiveMineAnimator.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class ExplosiveMineAnimator : MonoBehaviour
+{
+    
+        #region Variables
+
+    [Header("Sound")]
+    [SerializeField] private AudioClip beepSFX;
+
+    // Script-side
+    private AudioSource source;
+
+        #endregion
+
+    void Start() {
+
+        source = GetComponent<AudioSource>();
+
+    }
+
+    public void BeepSFX() {
+        source.PlayOneShot(beepSFX);
+    }
+
+}
---
+++ Assets/Scripts/Enemys/ExplosiveMine/ExplosiveMineBehaviour.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CircleCollider2D))]
+public class ExplosiveMineBehaviour : MonoBehaviour
+{
+    
+        #region Variables
+
+    [Header("Explosion")]
+    [SerializeField] private GameObject explosion;
+
+    [Header("Animation")]
+    [SerializeField] private Animator anim;
+
+    // Script-side variables
+    private GameObject player;
+    private bool insideRadius;
+    private float radius;
+
+        #endregion
+
+    void Start() {
+
+        player = GameManager.Player.gameObject;
+
+        radius = GetComponent<CircleCollider2D>().radius;
+
+    }
+
+    void Update() {
+
+        if(!insideRadius) return;
+
+        float distance = Vector2.Distance(this.transform.position, player.transform.position);
+
+        if(distance < 1.5)
+        {
+            Instantiate(explosion, transform.position, transform.rotation);
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            float velocityAnimation = 10 - distance;
+
+            anim.SetFloat("BeepSpeed", velocityAnimation);
+        }
+
+
+    }
+
+    void OnTriggerEnter2D(Collider2D col) {
+
+        if(col.gameObject == player)
+        {
+            anim.SetBool("Show", true);
+            insideRadius = true;
+        }
+
+    }
+
+    void OnTriggerExit2D(Collider2D col) {
+
+        if(col.gameObject == player)
+        {
+            anim.SetBool("Show", false);
+            insideRadius = false;
+        }
+
+    }
+
+}
