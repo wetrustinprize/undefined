0579a52d9fbd184040b02bb0502e9e2aeb47545d
Peterson <contact.petersonac@gmail.com>
2020-06-03 23:41:33 -0300

    New skil enabler module

---
+++ Assets/Scripts/Mechanics/SkillEnabler/SkillEnabler.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class SkillEnabler : MonoBehaviour
+{
+
+    public enum SkillToEnable {
+        Explosion,
+        Dash,
+        Teleport
+    }
+
+        #region Variables
+
+    [Header("Skill to enable")]
+    [SerializeField] private SkillToEnable skill = SkillToEnable.Explosion;
+
+        #endregion
+
+    void OnTriggerEnter2D(Collider2D col) {
+
+        if(col.tag == "Player")
+        {
+            PlayerController controller = GameManager.Player;
+
+            switch(skill)
+            {
+                case SkillToEnable.Dash:
+                    controller.canDash = true;
+                    break;
+                
+                case SkillToEnable.Explosion:
+                    controller.canExplode = true;
+                    break;
+                
+                case SkillToEnable.Teleport:
+                    controller.canTeleport = true;
+                    break;
+            }
+        }
+
+    }
+
+}
