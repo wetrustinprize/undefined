8cce35c53d9032c59dce16ee7e99f61d15607e5e
Peterson <contact.petersonac@gmail.com>
2020-06-05 01:57:01 -0300

    New item visual effects

---
+++ Assets/Scripts/Player/Effects/PlayerRainbow.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+public class PlayerRainbow : MonoBehaviour
+{
+
+        #region Variables
+
+    [Header("Trails")]
+    [SerializeField] private TrailRenderer[] trails = null;
+
+    // Script side
+    private GameObject myPlayer = null;
+
+        #endregion
+
+    public void Setup(GameObject player)
+    {
+        
+        myPlayer = player;
+
+        Jump jump = player.GetComponent<Jump>();
+        Motor motor = player.GetComponent<Motor>();
+
+        jump.OnWallJump += WallJump;
+        motor.onTouchGround += OnTouchGround;
+
+        ChangeTrail(false);
+
+    }
+
+    void OnDestroy() {
+
+        Jump jump = myPlayer.GetComponent<Jump>();
+        Motor motor = myPlayer.GetComponent<Motor>();
+
+        jump.OnWallJump -= WallJump;
+        motor.onTouchGround -= OnTouchGround;
+
+    }
+
+    void ChangeTrail(bool emit) {
+
+        foreach(TrailRenderer tr in trails)
+        {
+            tr.emitting = emit;
+        }
+
+    }
+
+    void WallJump() {
+
+        ChangeTrail(true);
+
+    }
+
+    void OnTouchGround() {
+
+        ChangeTrail(false);
+
+    }
+
+}
---
+++ Assets/Scripts/Player/Effects/PlayerShield.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class PlayerShield : MonoBehaviour
+{
+    
+        #region Variables
+
+    [Header("Animation")]
+    [SerializeField] private Animator animator = null;
+
+    // Script side
+    private GameObject myPlayer = null;
+
+        #endregion
+    
+    public void Setup(GameObject player) {
+
+        myPlayer = player;
+
+        Alive alive = player.GetComponent<Alive>();
+
+        alive.onDamage += Damaged;
+    }
+
+    void Damaged(int damage, GameObject dealer) {
+
+        Vector2 size = dealer.GetComponent<BoxCollider2D>().size / 2;
+        Vector2 offset = dealer.GetComponent<BoxCollider2D>().offset;
+
+        Vector2 dir = ((Vector2)dealer.transform.position - (Vector2)this.transform.position).normalized;
+
+        Debug.Log($"Player Pos: {myPlayer.transform.position}\nAtacker Pos: {dealer.transform.position}\nDirection: {dir}");
+
+        float rot_z = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        this.transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
+
+        animator.SetTrigger("Blink");
+
+    }
+
+    void OnDestroy() {
+
+        Alive alive = myPlayer.GetComponent<Alive>();
+
+        alive.onDamage -= Damaged;
+
+    }
+
+}
