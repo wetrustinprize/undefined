bd5126d1eaf2fc087b8c25f1b2d73634a07be5d4
Peterson <contact.petersonac@gmail.com>
2020-06-05 01:55:41 -0300

    New equipeditems HUD class

--- Assets/Scripts/HUD/HUDEquipedItems.cs
+++ Assets/Scripts/HUD/HUDEquipedItems.cs
@@ -4,10 +4,49 @@ using UnityEngine.UI;
 public class HUDEquipedItems : MonoBehaviour
 {
     
-        #region Variables
+                #region Variables
 
-    
+        [Header("Images")]
+        [SerializeField] private Image passiveIcon = null;
+        [SerializeField] private Text passiveQuantity = null;
 
-        #endregion
+        [Space(10)]
+        [SerializeField] private Image activeIcon = null;
+
+        // Script side
+        PlayerInventory myInventory = null;
+
+                #endregion
+
+        void Start() {
+
+                myInventory = GameManager.Player.GetComponent<PlayerInventory>();
+
+                myInventory.onEquip += cb => { Refresh(); };
+                myInventory.onUnequip += cb => { Refresh(); };
+                myInventory.onUse += cb => { Refresh(); };
+
+        }
+
+        void Refresh() {
+
+                if(myInventory.PassiveItem.itemObj != null)
+                        passiveIcon.sprite = myInventory.PassiveItem.itemObj.icon;
+                else
+                        passiveIcon.sprite = null;
+
+
+                if(myInventory.ActiveItem.itemObj != null)
+                {                        
+                        activeIcon.sprite = myInventory.ActiveItem.itemObj.icon;
+                        passiveQuantity.text = $"x{myInventory.ActiveItem.quantity}";
+                }
+                else
+                {
+                        activeIcon.sprite = null;
+                        passiveQuantity.text = $"";
+                }
+
+        }
 
 }
