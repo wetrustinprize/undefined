ab573477e95b4f3c0d8e51ca302d1d827c59b2b2
Peterson <contact.petersonac@gmail.com>
2020-06-11 22:35:28 -0300

    new coin pickup

---
+++ Assets/Scripts/Mechanics/SecretCoin/SecretCoinBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using Undefined.Items.Shop;
+
+public class SecretCoinBehaviour : MonoBehaviour
+{
+    
+        #region Variables
+
+    [Header("Sounds")]
+    [SerializeField] private AudioClip pickupSound = null;
+
+    [Header("Animator")]
+    [SerializeField] private Animator animator = null;
+
+        #endregion
+
+    void OnTriggerEnter2D(Collider2D col) {
+
+        if(col.tag == "Player")
+        {
+            animator.SetTrigger("Pick");
+            col.GetComponent<PlayerInventory>().AddGold(1, ShopCoin.Secret);
+            GameManager.Sound.PlayUISFX(pickupSound, 1);
+            Destroy(this.gameObject, 1);
+        }
+
+    }
+
+}
